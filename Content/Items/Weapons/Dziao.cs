dc941eaa042a07bb4a5570c899a2b9f4555f652a
Dominik G <domiran35@gmail.com>
2023-04-08 13:03:23 +0200

    brothers weapon

---
+++ Content/Items/Weapons/Dziao.cs
@@ -0,0 +1,38 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Teaser.Content.Items.Weapons
+{
+	public class Dziao : ModItem
+	{
+		public override void SetDefaults() {
+			Item.width = 62;
+			Item.height = 32;
+			Item.scale = .3f;
+			Item.rare = ItemRarityID.Green;
+
+			Item.useTime = 4;
+			Item.useAnimation = 8;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.autoReuse = true;
+			
+			Item.DamageType = DamageClass.Ranged;
+			Item.damage = 200;
+			Item.knockBack = 5f;
+			Item.noMelee = true;
+
+			Item.shoot = ProjectileID.PurificationPowder;
+			Item.shootSpeed = 40f;
+			Item.useAmmo = AmmoID.None;
+        }
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes() {
+			CreateRecipe()
+				.AddIngredient(ItemID.Wood, 1)
+				.AddTile(TileID.WorkBenches)
+				.Register();
+		}
+    }
+}
